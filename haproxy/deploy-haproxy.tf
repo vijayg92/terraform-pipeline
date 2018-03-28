1d7cf4dbcbea0eeeabd409f9259c2be6221e1011
Vijay Gosai <vgosai@redhat.com>
2018-03-28 15:46:36 +0530

    Added HAProxy Sample Terrform Template

---
+++ haproxy/deploy-haproxy.tf
@@ -0,0 +1,150 @@
+###################################################################################################################
+##  Variables
+###################################################################################################################
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+variable "private_key_path" {}
+variable "key_name" {
+  default = "DevOpsUser"
+}
+variable "cidr_address_space" {
+  default = "172.21.0.0/16"
+}
+variable "subnet1_address_space" {
+  default = "172.21.1.0/24"
+}
+variable "subnet2_address_space" {
+  default = "172.21.2.0/24"
+}
+
+###################################################################################################################
+##  Providers
+###################################################################################################################
+
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.aws_region}"
+}
+
+###################################################################################################################
+##  Data
+###################################################################################################################
+
+data "aws_availability_zones" "available" {}
+
+###################################################################################################################
+##  Resources
+###################################################################################################################
+
+## Networking ##
+resource "aws_vpc" "vpc" {
+    cidr_block = "${var.cidr_address_space}"
+    enable_dns_hostnames = "true"
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = "${aws_vpc.vpc.id}"
+}
+
+resource "aws_subnet" "subnet1" {
+    cidr_block = "${var.subnet1_address_space}"
+    vpc_id =  "${aws_vpc.vpc.id}"
+    map_public_ip_on_launch = true
+    availability_zone = "${data.aws_availability_zones.available.names[0]}"
+}
+
+resource "aws_subnet" "subnet2" {
+    cidr_block = "${var.subnet2_address_space}"
+    vpc_id =  "${aws_vpc.vpc.id}"
+    map_public_ip_on_launch = true
+    availability_zone = "${data.aws_availability_zones.available.names[1]}"
+}
+
+## Routing ##
+resource "aws_route_table" "rt" {
+    vpc_id = "${aws_vpc.vpc.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.igw.id}"
+    }
+}
+
+resource "aws_route_table_association" "rt-subnet1" {
+    subnet_id = "${aws_subnet.subnet1.id}"
+    route_table_id = "${aws_route_table.rt.id}"
+}
+
+resource "aws_route_table_association" "rt-subnet2" {
+    subnet_id = "${aws_subnet.subnet2.id}"
+    route_table_id = "${aws_route_table.rt.id}"
+}
+
+## Security Group ##
+resource "aws_security_group" "nginx-sg" {
+
+    name = "nginx-sg"
+    vpc_id = "${aws_vpc.vpc.id}"
+
+    ingress{
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress{
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_eip" "haproxy-eip" {
+  vpc = true
+}
+
+## Instance ##
+resource "aws_instance" "nginx" {
+
+    ami = "ami-7c87d913"
+    instance_type = "t2.micro"
+    subnet_id = "${aws_subnet.subnet1.id}"
+    vpc_security_group_ids = ["${aws_security_group.nginx-sg.id}"]
+    key_name = "${var.key_name}"
+
+    connection {
+      user = "ec2-user"
+      private_key = "${file(var.private_key_path)}"
+    }
+
+    provisioner "remote-exec" {
+      inline = [
+      "sudo yum install -y haproxy",
+      "sudo service haproxy start"
+      ]
+    }
+}
+
+resource "aws_eip_association" "haproxy_eip" {
+  instance_id   = "${aws_instance.nginx.id}"
+  allocation_id = "${aws_eip.haproxy-eip.id}"
+}
+###################################################################################################################
+##  Output
+###################################################################################################################
+
+output "aws_instance_public_dns" {
+    value = "${aws_instance.nginx.public_dns}"
+}
--- nginx/deploy-nginx.tf
+++ nginx/deploy-nginx.tf
@@ -4,9 +4,19 @@
 
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
+#variable "aws_region" {}
 variable "private_key_path" {}
 variable "key_name" {
-  default = "KopsUser"
+  default = "DevOpsUser"
+}
+variable "cidr_address_space" {
+  default = "172.21.0.0/16"
+}
+variable "subnet1_address_space" {
+  default = "172.21.1.0/24"
+}
+variable "subnet2_address_space" {
+  default = "172.21.2.0/24"
 }
 
 ###################################################################################################################
@@ -16,17 +26,100 @@ variable "key_name" {
 provider "aws" {
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
+    #region = "${var.aws_region}"
     region = "ap-south-1"
 }
+
+###################################################################################################################
+##  Data
+###################################################################################################################
+
+data "aws_availibility_zones" "available" {}
 
 ###################################################################################################################
 ##  Resources
 ###################################################################################################################
 
+## Networking ##
+resource "aws_vpc" "vpc" {
+    cidr_block = "${var.cidr_address_space}"
+    enable_dns_hostname = "true"
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = "${aws_vpc.vpc.id}"
+}
+
+resource "aws_subnet" "subnet1" {
+    cidr_block = "${var.subnet1_address_space}"
+    vpc_id =  "${aws_vpc.vpc.id}"
+    map_public_ip_on_lunch = "true"
+    avalibility_zone = "${data.aws_availibility_zones.available.names[0]}"
+}
+
+resource "aws_subnet" "subnet2" {
+    cidr_block = "${var.subnet2_address_space}"
+    vpc_id =  "${aws_vpc.vpc.id}"
+    map_public_ip_on_lunch = "true"
+    avalibility_zone = "${data.aws_availibility_zones.available.names[1]}"
+}
+
+## Routing ##
+resource "aws_route_table" "rt" {
+    vpc_id = "${aws_vpc.vpc.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.igw.id}"
+    }
+}
+
+resource "aws_route_table_association" "rt-subnet1" {
+    subnet_id = "${aws_subnet.subnet1.id}"
+    route_table_id = "${aws_route_table.rt.id}"
+}
+
+
+resource "aws_route_table_association" "rt-subnet2" {
+    subnet_id = "${aws_subnet.subnet2.id}"
+    route_table_id = "${aws_route_table.rt.id}"
+}
+
+## Security Group ##
+resource "aws_security_group" "nginx-sg" {
+
+    name = "nginx-sg"
+    vpc_id = "${aws_vpc.vpc.id}"
+
+    ingress{
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress{
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+## Instance ##
 resource "aws_instance" "nginx" {
 
     ami = "ami-7c87d913"
     instance_type = "t2.micro"
+    subnet_id = "${aws_subnet.subnet1.id}"
+    vpc_security_group = "[${aws_security_group.nginx-sg.id}]"
     key_name = "${var.key_name}"
 
     connection {
@@ -37,6 +130,7 @@ resource "aws_instance" "nginx" {
     provisioner "remote-exec" {
       inline = [
       "sudo yum install -y nginx",
+      "echo '<html><head><title>VJ</title></head><body><Hello World></body></html>'",
       "sudo service nginx start"
       ]
     }
